7d20a0d5a39505438c525cb4a7e21582fee1e569
GoddoNebianU <goddonebianu@outlook.com>
2024-12-25 20:24:50 +0800

    用状态机进行重构，增加单词重现功能

--- translation.py
+++ translation.py
@@ -46,25 +46,69 @@ def translation(silent_mode=False, multithreaded=True):
     global MULTITHREADED
     MULTITHREADED = multithreaded
 
+    class Flag(Enum):
+        EXIT = 0
+        BLANK = 1
+        NEXT = 2
+        READ = 3
+        READED = 4
+
+    rollback_words = []
+    last_word = None
     while True:
-        print('\n----------------------------------------------------------------------\n')
+        print('\n\n------------------------------------------------------------------------------------\n\n')
+
 
         word = random.choice(data)
-        print(word['word'], end='\t\t\t')
+        for i, v in enumerate(rollback_words):
+            if v[0] <= 0:
+                word = v[1]
+                del rollback_words[i]
+                break
+        print(word['word'])
         if not SILENT_MODE: speak_word(word['word'])
-        else: print('\n'+word['usphone'], end='')
+        else: print(word['usphone'])
 
-        ins = input()
-        if ins in ['q', 'quit']:
-            break
-        if not ins:
-            print(word['trans'][0]['tranCn'])
-            continue
+        ins = ''
+        flag = Flag.READ
+        while True:
+            match flag:
+                case Flag.BLANK:
+                    continue
+                case Flag.NEXT:
+                    for i, v in enumerate(rollback_words):
+                        rollback_words[i][0] -= 1
+                    break
+                case Flag.READ:
+                    ins = input()
+                    flag = Flag.READED
+                case Flag.READED:
+                    if ins == 'rb' and last_word is not None:
+                        # 单词重现
+                        rollback_words.append([5, last_word])
+                        print(f'单词{last_word['word']}将在5回合后重现。')
+                        flag = Flag.READ
+                    elif ins in ['q', 'quit']:
+                        # 退出程序
+                        exit()
+                    elif not ins:
+                        # 输出中文翻译
+                        print(word['trans'][0]['tranCn'])
+                        last_word = word
+                        flag = Flag.NEXT
+                    elif ins in ['\'', '\\', ']', '/']:
+                        # 输出单词详情，朗读中文翻译
+                        print(json.dumps(word['trans'], indent=4, ensure_ascii=False))
+                        print(word['sentences'][0]['sContent'])
+                        if not SILENT_MODE: speak_zh(word['trans'][0]['tranCn'])
+                        flag = Flag.NEXT
+                    else:
+                        flag = Flag.READ
 
-        print(json.dumps(word['trans'], indent=4, ensure_ascii=False))
-        print(word['sentences'][0]['sContent'], end='')
-        if not SILENT_MODE: speak_zh(word['trans'][0]['tranCn'])
-        input()
 
 if __name__ == '__main__':
     translation()
+
+    # if ins == 'rb':
+    #     rollback_words.append((5, word['word']))
+    #     print(f'单词{word['word']}将在5回合后重现。')
